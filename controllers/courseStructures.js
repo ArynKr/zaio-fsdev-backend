669e433153c1b3b01126adb8f2db78db874642e6
Aryan Kumar <aryankumar1504@gmail.com>
2023-04-06 23:41:34 +0530

    Updated course struc. returned form an array to an object

--- controllers/courseStructures.js
+++ controllers/courseStructures.js
@@ -4,7 +4,10 @@ exports.getCourseStructure = async (req, res) => {
   const { hoursPerDay } = req.query;
   try {
     const courseStructures = await CourseStructure.find({ hoursPerDay });
-    res.json(courseStructures)
+    if (courseStructures.length === 0) {
+      res.status(404).json({ message: 'Course structure not found' });
+    }
+    res.json(courseStructures[0])
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: err.message })
