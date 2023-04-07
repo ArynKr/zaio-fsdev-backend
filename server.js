8d9f121fdd9a55a5afcba0d8b0d8a6e2e160db9f
Aryan Kumar <aryankumar1504@gmail.com>
2023-04-07 17:22:55 +0530

    Added cors for cross origin access

--- server.js
+++ server.js
@@ -16,22 +16,8 @@ const PORT = process.env.PORT || 5000
 /* middleware body parser */
 app.use(express.json())
 
-/* enable CORS */
-const whitelist = ['https://zaio-challenge.netlify.app']
-const corsOptions = {
-  origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-          callback(null, true)
-      } else {
-          callback(new Error('Not allowed by CORS'))
-      }
-  },
-}
-if (process.env.NODE_ENV === 'development') {
-  app.use(cors())
-} else {
-  app.use(cors(corsOptions))
-}
+/* allow access using  cors */
+app.use(cors())
 
 /* routes */
 app.get('/api/health-check', (_req, res) => res.send('Health OK'))
